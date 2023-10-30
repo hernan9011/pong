cf1befd2d424b46fb83841bea506c36feb7a6876
hernan9011 <pipivr72@gmail.com>
2023-10-30 14:39:25 -0300

    web

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const socket = io('http://localhost:3000', { transports: ["websocket"] });
+const socket = io('https://pong2023.netlify.app/', { transports: ["websocket"] });
 
 const paddleWidth = 10;
 const paddleHeight = 60;
