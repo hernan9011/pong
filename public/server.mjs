f1d3b881df7907603ab2ba0bfc561ae1f9dccae9
hernan9011 <pipivr72@gmail.com>
2023-11-01 15:52:30 -0300

    fix

--- public/server.mjs
+++ public/server.mjs
@@ -1,7 +1,6 @@
-import { Server } from 'socket.io';
-import express from 'express';
-import { createServer } from 'node:http';
-
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
