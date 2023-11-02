7b74e44d83edc3ec5ed6fbce2cc0ec04dd73dd42
hernan9011 <pipivr72@gmail.com>
2023-11-01 23:48:41 -0300

    fix

--- public/server.mjs
+++ public/server.mjs
@@ -1,11 +1,10 @@
-import { Server } from 'socket.io';
-import express from 'express';
-import { createServer } from 'node:http';
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
 //app.use(http.middleware);                 // Add serverless-http middleware
 //exports.handler = netlifyLambda.handler;  // Export Netlify Lambda handler
 
