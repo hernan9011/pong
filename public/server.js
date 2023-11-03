78cbc21e463a2e60c6b7588e2cebb2eabbd125fc
hernan9011 <pipivr72@gmail.com>
2023-11-03 00:01:20 -0300

    fix

--- public/server.js
+++ public/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { path } from 'path';
 
 const app = express();
 const server = createServer(app);
