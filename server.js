7bf38f29a54710613f4b86f833a76eea2b14bbb7
hernan9011 <pipivr72@gmail.com>
2023-11-03 12:07:17 -0300

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server);
 const port = 3000 || process.env.PORT;
 // Definir una ruta de prueba
 app.get('/', (req, res) => {
-    res.sendFile("index.html");
+    res.sendFile(new URL('./index.html', import.meta.url).pathname);
   });
 
 // Iniciar el servidor
