f2ec4ccc4a17b711064ceca9c6661f7b567bbda5
hernan9011 <pipivr72@gmail.com>
2023-11-03 15:16:48 -0300

    fix

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
+import { io } from "socket.io-client";
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const socket = io('http://localhost:3000', { transports: ["websocket"] });
+const socket = io('https://pong2023.netlify.app/socket.io/');
 
 const paddleWidth = 10;
 const paddleHeight = 60;
