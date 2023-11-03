acedd1d063f944b0603a206a3e55495746083593
hernan9011 <pipivr72@gmail.com>
2023-11-02 22:01:33 -0300

    fix

--- public/server.js
+++ public/server.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { Server } = require('socket.io');
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-//app.use(http.middleware);                 // Add serverless-http middleware
-//exports.handler = netlifyLambda.handler;  // Export Netlify Lambda handler
 
 const PlayerServer = {};
 const ballServer = {
