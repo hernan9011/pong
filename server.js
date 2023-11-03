e9c16300081e7ed5bbbe2ec163f6342bd8f97179
hernan9011 <pipivr72@gmail.com>
2023-11-03 11:32:47 -0300

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
